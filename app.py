d356f8f7358d621976aa1d78a65c8d1bd6375add
bonganisibanda <sibandabongz@gmail.com>
2017-02-04 22:48:39 +0200

    added index.html+jinja2

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 from redis import Redis
 
 app = Flask(__name__)
 redis = Redis(host='redis', port=6379)
 
+
 @app.route('/')
 def hello():
     count = redis.incr('hits')
-    return 'Hello World! I have been seen {} times.\n'.format(count)
+    return render_template('index.html', count=count)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
